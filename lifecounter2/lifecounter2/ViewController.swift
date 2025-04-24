93698eb1e722877834e7ce78b6cd35b86abd8622
bella-gatz <102693135+bella-gatz@users.noreply.github.com>
2025-04-23 19:51:03 -0700

    adding half functionality

--- lifecounter2/lifecounter2/ViewController.swift
+++ lifecounter2/lifecounter2/ViewController.swift
@@ -7,23 +7,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        p1Text.delegate = self
+        p1Text.keyboardType = .numberPad
+        p2Text.delegate = self
+        p2Text.keyboardType = .numberPad
+        addPlayerButton.isHidden = false
         // Do any additional setup after loading the view.
     }
     
     var player1Lives = 20
     var player2Lives = 20
-
+    var numPlayers = 4
+    var players : [player] = [] // add first four players
+    var gameStart = false // TODO: add true when life totals changed
+    
     @IBOutlet weak var player1LifeLabel: UILabel!
     
     @IBOutlet weak var player2LifeLabel: UILabel!
     
     @IBOutlet weak var loserLabel: UILabel!
 
-    // player 1 buttons
+        
+    @IBOutlet weak var addPlayerButton: UIButton!
+    
+    @IBAction func addPlayer(_ sender: Any) {
+        if numPlayers < 8 {
+            numPlayers += 1
+            players.append(player(numPlayers))
+            print(numPlayers)
+        } else {
+            addPlayerButton.isHidden = true
+        }
+    }
+    
+    // TODO: add func for four players --> add to class?
+    
+    // player 1
     @IBAction func p1plus1(_ sender: Any) {
         updateLives(player: 1, lives: 1)
     }
@@ -32,15 +55,25 @@ class ViewController: UIViewController {
         updateLives(player: 1, lives: -1)
     }
     
-    @IBAction func p1plus5(_ sender: Any) {
-        updateLives(player: 1, lives: 5)
+    @IBOutlet weak var p1Text: UITextField!
+
+    @IBOutlet weak var p1plusminus: UISegmentedControl!
+        
+    @IBAction func p1LifeReq(_ sender: Any) {
+        if let text = p1Text.text, let number = Int(text) {
+            if p1plusminus.selectedSegmentIndex == 1 {
+                updateLives(player: 1, lives: number * -1)
+            } else {
+                updateLives(player: 1, lives: number)
+            }
+        } else {
+             print("Please enter a valid number")
+        }
+        p1Text.text = ""
     }
+
+    // player 2
     
-    @IBAction func p1minus5(_ sender: Any) {
-        updateLives(player: 1, lives: -5)
-    }
-    
-    // player 2 buttons
     @IBAction func p2plus1(_ sender: Any) {
         updateLives(player: 2, lives: 1)
     }
@@ -48,16 +81,29 @@ class ViewController: UIViewController {
     @IBAction func p2minus1(_ sender: Any) {
         updateLives(player: 2, lives: -1)
     }
+
+    @IBOutlet weak var p2Text: UITextField!
+
+    @IBOutlet weak var p2plusminus: UISegmentedControl!
     
-    @IBAction func p2plus5(_ sender: Any) {
-        updateLives(player: 2, lives: 5)
-    }
-    
-    @IBAction func p2minus5(_ sender: UIButton) {
-        updateLives(player: 2, lives: -5)
+    @IBAction func p2LifeReq(_ sender: Any) {
+        if let text = p2Text.text, let number = Int(text) {
+            if p2plusminus.selectedSegmentIndex == 1 {
+                updateLives(player: 2, lives: number * -1)
+            } else {
+                updateLives(player: 2, lives: number)
+            }
+        } else {
+             print("Please enter a valid number")
+        }
+        p2Text.text = ""
     }
     
     func updateLives(player p : Int, lives l : Int) {
+        gameStart = true
+        if gameStart == true {
+            addPlayerButton.isHidden = true
+        }
         // updates lives
         if p == 1 {
             player1Lives += l
@@ -70,13 +116,28 @@ class ViewController: UIViewController {
         // checks for loss
         if (player1Lives <= 0) {
             loserLabel.text = "Player 1 LOSES!"
+            print("Player 1 LOSES!")
+            addPlayerButton.isHidden = false
+
         } else if player2Lives <= 0 {
             loserLabel.text = "Player 2 LOSES!"
+            print("Player 2 LOSES!")
+            addPlayerButton.isHidden = false
         } else {
             loserLabel.text = ""
         }
     }
     
+}
+
+class player {
+    var num: Int
+    var lives : Int
+    
+    init(_ num : Int) {
+        self.num = num
+        self.lives = 20
+    }
 }
 
 
